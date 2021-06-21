35dfe96b3745452978bf3b57842ff8e32e5931da
Bruno Wallace <Bruwallaceag@gmail.com>
2021-06-21 18:14:40 -0500

    2°Commit
    Nav-bar y header sin imagenes

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    lightBg: false,
+    primary: true,
+    lightBg: false, 
+    imgStart: '', 
+    lightTopLine: true, 
+    lightTextDesc: true, 
+    buttonLabel: 'Get Started', 
+    description: 'We helps business owners increase their revenue. Our team of unique specialist can help achive your business goals',
+    headLine: 'Lead generation specialist for online Business', 
+    lightText: true, 
+    topLine: 'Marketing Agency',
+    img: '',
+    alt: 'Image',
+    start: ''
+}
